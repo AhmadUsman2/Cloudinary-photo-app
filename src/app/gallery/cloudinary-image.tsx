e581015157d09b994ebb715490aec56979c21fab
Ahmad Usman <140284351+AhmadUsman2@users.noreply.github.com>
2024-02-10 11:50:25 +0500

    images on gallery page

---
+++ src/app/gallery/cloudinary-image.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { CldImage } from "next-cloudinary"
+
+export default function CloudinaryImage(props: any) {
+    return (
+        <CldImage
+             
+              {...props}
+            
+            />
+    )
+}
